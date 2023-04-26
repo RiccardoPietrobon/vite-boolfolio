bb0e078bde596f95133796e1071ca59f7c850c70
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-04-26 17:41:23 +0200

    router

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -16,7 +16,7 @@ export default {
 <template>
     <div>
         <div class="card" style="width: 18rem;">
-            <img src="{{ project.getImageUri() }}" class="card-img-top" alt="...">
+            <img src="{{ project.image }}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ project.title }}</h5>
                 <p class="card-text">{{ abstract }}</p>
